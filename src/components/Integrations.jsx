5cc58b99da83b8915762c7677036b1aec1fc2fef
BILLEL <billel.bahani@gmail.com>
2022-04-17 13:55:37 +0100

    fixiing somme isues

--- src/components/Integrations.jsx
+++ src/components/Integrations.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   Container,
   makeStyles,
@@ -10,42 +10,42 @@ import {
   Button,
   ButtonGroup,
   Box,
-  TextField
-} from "@material-ui/core";
+  TextField,
+} from '@material-ui/core';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import LibraryAddCheckRoundedIcon from '@mui/icons-material/LibraryAddCheckRounded';
 
 const useStyles = makeStyles((theme) => ({
   integrations: {
-    padding: "2.5rem",
+    padding: '2.5rem',
   },
   innerWrapper: {
     // padding:"3rem",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-    height: "80vh",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    // height: "80vh",
   },
   detail: {
-    width: "65%",
-    height: "100%",
-    backgroundColor: "#fff",
-    borderRadius: "4px",
+    width: '65%',
+    height: '100%',
+    backgroundColor: '#fff',
+    borderRadius: '4px',
     boxShadow:
-      "0px 2px 1px -1px rgba(0,0,0,.2) ,0px 1px 1px 0px rgba(0,0,0,.14),0px 1px 3px 0px rgba(0,0,0,.12)",
-    padding:"3rem"  
+      '0px 2px 1px -1px rgba(0,0,0,.2) ,0px 1px 1px 0px rgba(0,0,0,.14),0px 1px 3px 0px rgba(0,0,0,.12)',
+    padding: '3rem',
   },
   info: {
-    width: "30%",
-    height: "100%",
-    borderRadius: "4px",
+    width: '30%',
+    height: '100%',
+    borderRadius: '4px',
     boxShadow:
-      "0px 2px 1px -1px rgba(0,0,0,.2) ,0px 1px 1px 0px rgba(0,0,0,.14),0px 1px 3px 0px rgba(0,0,0,.12)",
-    backgroundColor: "#fff",
-    padding:"1rem"
+      '0px 2px 1px -1px rgba(0,0,0,.2) ,0px 1px 1px 0px rgba(0,0,0,.14),0px 1px 3px 0px rgba(0,0,0,.12)',
+    backgroundColor: '#fff',
+    padding: '6rem 2rem',
   },
 }));
 
@@ -57,9 +57,9 @@ const Integrations = () => {
         variant="h4"
         component="div"
         gutterBottom
-        style={{ fontWeight: "300" }}
+        style={{ fontWeight: '300' }}
       >
-        Connect to website{" "}
+        Connect to website{' '}
       </Typography>
       <Box className={classes.innerWrapper}>
         <Box className={classes.detail}>
@@ -67,79 +67,100 @@ const Integrations = () => {
             variant="h6"
             component="div"
             gutterBottom
-            style={{ fontWeight: "500" }}
+            style={{ fontWeight: '500' }}
           >
             How to connect SALMADJAREB to your website
           </Typography>
-          <List gutterBottom>
-          </List>
+          <List gutterBottom></List>
           <ListItem>Enter your Website domain and click Connect</ListItem>
-          <ListItem>Confirm that you want to integrate with SALMDJAREB</ListItem>
-          <ListItem>Give SALMDJAREB permission to connect with your website</ListItem>
-              
+          <ListItem>
+            Confirm that you want to integrate with SALMDJAREB
+          </ListItem>
+          <ListItem>
+            Give SALMDJAREB permission to connect with your website
+          </ListItem>
+
           <Typography
             variant="h6"
             component="div"
             gutterBottom
-            style={{ fontWeight: "500",marginTop:"1rem" }}
+            style={{ fontWeight: '500', marginTop: '1rem' }}
           >
-           Enter Your Website domain
+            Enter Your Website domain
           </Typography>
           <TextField
-          id="standard-search"
-          label="yourwebsite.salmdjareb.com "
-          type="search"
-          variant="outlined"
-          fullWidth
-          className={classes.searchInput}
-          gutterBottom
-        />
-        <Button variant="contained" color="secondary" style={{marginTop:"1rem"}}>Connect</Button>
+            id="standard-search"
+            label="yourwebsite.salmdjareb.com "
+            type="search"
+            variant="outlined"
+            fullWidth
+            className={classes.searchInput}
+            gutterBottom
+          />
+          <Button
+            variant="contained"
+            color="secondary"
+            style={{ marginTop: '1rem' }}
+          >
+            Connect
+          </Button>
         </Box>
         <Box className={classes.info}>
-        <Typography
+          <Typography
             variant="h6"
             component="div"
             gutterBottom
-            style={{ fontWeight: "500",marginTop:"1rem" }}
+            style={{ fontWeight: '500', marginTop: '1rem' }}
           >
-           Main benefits:
+            Main benefits:
           </Typography>
           <ListItem>
-              <LibraryAddCheckRoundedIcon style={{color:"greenyellow"}}/>
-              <Typography
-            variant="h6"
-            component="div"
-            gutterBottom
-            style={{ fontWeight: "300",marginTop:"1rem" ,marginLeft:".5rem" , fontSize:"1rem"}}
+            <LibraryAddCheckRoundedIcon style={{ color: 'greenyellow' }} />
+            <Typography
+              variant="h6"
+              component="div"
+              gutterBottom
+              style={{
+                fontWeight: '300',
+                marginTop: '1rem',
+                marginLeft: '.5rem',
+                fontSize: '1rem',
+              }}
             >
               Automate review request.
-           
-          </Typography>
+            </Typography>
           </ListItem>
           <ListItem>
-              <LibraryAddCheckRoundedIcon style={{color:"greenyellow"}}/>
-              <Typography
-            variant="h6"
-            component="div"
-            gutterBottom
-            style={{ fontWeight: "300",marginTop:"1rem" ,marginLeft:".5rem" , fontSize:"1rem"}}
+            <LibraryAddCheckRoundedIcon style={{ color: 'greenyellow' }} />
+            <Typography
+              variant="h6"
+              component="div"
+              gutterBottom
+              style={{
+                fontWeight: '300',
+                marginTop: '1rem',
+                marginLeft: '.5rem',
+                fontSize: '1rem',
+              }}
             >
               Send requests to past custumers in one go!
-           
-          </Typography>
+            </Typography>
           </ListItem>
           <ListItem>
-              <LibraryAddCheckRoundedIcon style={{color:"greenyellow"}}/>
-              <Typography
-            variant="h6"
-            component="div"
-            gutterBottom
-            style={{ fontWeight: "300",marginTop:"1rem" ,marginLeft:".5rem", fontSize:"1rem"}}
+            <LibraryAddCheckRoundedIcon style={{ color: 'greenyellow' }} />
+            <Typography
+              variant="h6"
+              component="div"
+              gutterBottom
+              style={{
+                fontWeight: '300',
+                marginTop: '1rem',
+                marginLeft: '.5rem',
+                fontSize: '1rem',
+              }}
             >
               Add multiple TrustBox widgets one-site.
-           
-          </Typography>
+            </Typography>
           </ListItem>
         </Box>
       </Box>
