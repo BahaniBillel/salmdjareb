8e187a5674c4e7792579dd87cd4989a96288cc77
BILLEL <billel.bahani@gmail.com>
2022-04-25 09:54:49 +0100

    ui

--- src/components/HomeProduct.jsx
+++ src/components/HomeProduct.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   Container,
   makeStyles,
@@ -10,149 +10,160 @@ import {
   Button,
   ButtonGroup,
   Box,
-} from "@material-ui/core";
-import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
-import { Link } from "react-router-dom";
-import Trust from "../images/trust.jpg";
-import Showcase from "../images/showcase.svg";
-import Showcase2 from "../images/showcase2.svg";
+} from '@material-ui/core';
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   homeProduct: {
-    width: "100%",
-    height: "120vh",
-    margin:"10rem 0"
+    width: '100%',
+    height: '120vh',
+    margin: '10rem 0',
   },
 
   innerWrapper: {
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#fcc8c2",
-    borderRadius: "4px",
-    padding: "3rem",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "flex-start",
-    justifyContent: "center",
-    flexWrap: "nowrap",
+    width: '100%',
+    height: '100%',
+    borderRadius: '4px',
+    padding: '3rem',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    flexWrap: 'nowrap',
   },
 }));
 
-const HomeProduct = () => {
+const HomeProduct = ({
+  title,
+  text1,
+  text2,
+  text3,
+  SMtitle1,
+  SMtitle2,
+  SMtitle3,
+  box1,
+  box2,
+  box3,
+  bkg1,
+  bkg2,
+  bigImg,
+  SMimg,
+  backColor,
+}) => {
   const classes = useStyles();
   return (
     <Box className={classes.homeProduct}>
-      <Container className={classes.innerWrapper}>
-        <Box style={{ width: "35%" }}>
-          <Typography variant="h2" style={{ fontWeight: "600" }}>
-            Trust.
+      <Container
+        className={classes.innerWrapper}
+        style={{ background: `${backColor}` }}
+      >
+        <Box style={{ width: '35%' }}>
+          <Typography variant="h2" style={{ fontWeight: '600' }}>
+            {title}
           </Typography>
           <Typography
             variant="body1"
             style={{
-              fontSize: "1.2rem",
-              lineHeight: "1.75rem",
-              fontWeight: "300",
-              padding: "0 3rem 0 0",
+              fontSize: '1.2rem',
+              lineHeight: '1.75rem',
+              fontWeight: '300',
+              padding: '0 3rem 0 0',
             }}
           >
-            Reviews help consumers buy with confidence - and 93% of online
-            shoppers say they impact their purchase decisions.
+            {text1}
             <br />
             <br />
             <br />
-            Boost your brand’s conversion with trust signals at every point of
-            contact.
+
+            {text2}
           </Typography>
           <FormatQuoteIcon
-            style={{ transform: "rotate(180deg)", fontSize: "3.5rem" }}
+            style={{ transform: 'rotate(180deg)', fontSize: '3.5rem' }}
           />
-          <Typography variant="h5" style={{padding:"0 2rem 0 0"}} component="div">
-            SAL-MADJAREB helped us build trust with customers, which has had a
-            positive impact on customer satisfaction and conversion.
+          <Typography
+            variant="h5"
+            style={{ padding: '0 2rem 0 0' }}
+            component="div"
+          >
+            {text3}
           </Typography>
         </Box>
 
         {/* Right side */}
         <Box
           style={{
-            width: "65%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            alignItems: "center",
+            width: '65%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
           }}
         >
           <Box
             style={{
-              height: "60%",
-              width: "100%",
-              background: "rgb(131,70,119)",
-              background:
-                "linear-gradient(286deg, rgba(131,70,119,1) 0%, rgba(249,86,79,1) 71%, rgba(230,230,230,1) 100%)",
-              padding: "1rem",
+              height: '60%',
+              width: '100%',
+              background: `${bkg1}`,
+              background: `${bkg2}`,
+              padding: '1rem',
             }}
           >
             <Box
               style={{
-                width: "60%",
-                transform: "translateY(-28%)",
-                background: "#fff",
-                padding: "1rem",
+                width: '60%',
+                transform: 'translateY(-28%)',
+                background: '#fff',
+                padding: '1rem',
               }}
             >
               <img
-                src={Trust}
+                src={bigImg}
                 alt=""
-                style={{ objectFit: "scale-down", width: "100%" }}
+                style={{ objectFit: 'scale-down', width: '100%' }}
               />
             </Box>
-            <Box style={{ width: "60%", transform: "translate(30%,-100%)" }}>
+            <Box style={{ width: '60%', transform: 'translate(30%,-100%)' }}>
               <img
-                src={Showcase}
+                src={SMimg}
                 alt=""
-                style={{ objectFit: "cover", width: "100%", height: "100%" }}
+                style={{ objectFit: 'cover', width: '100%', height: '100%' }}
               />
             </Box>
           </Box>
           <Box
             style={{
-              height: "40%",
-              width: "100%",
-              display: "flex",
-              flexDirection: "row",
-              justifyConent: "space-between",
-              padding:"2rem 0"
+              height: '40%',
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyConent: 'space-between',
+              padding: '2rem 0',
             }}
           >
-            <Box style={{width:"32%", padding:"1rem"}}>
+            <Box style={{ width: '32%', padding: '1rem' }}>
               <Typography variant="h6" component="div">
-                Product reviews
+                {SMtitle1}
               </Typography>
               <Typography variant="body1" component="div">
-                See greater conversion with product review throughout the buyer
-                journey. The more feedback you collect, the greater the SEO
-                benefits.
+                {box1}
               </Typography>
             </Box>
-            <Box  style={{width:"32%", padding:"1rem"}}>
+            <Box style={{ width: '32%', padding: '1rem' }}>
               <Typography variant="h6" component="div">
-                Product insights
+                {SMtitle2}
               </Typography>
               <Typography variant="body1" component="div">
-                Enhance the shopping experience and help customers choose the
-                right products for their needs with Reviewer attributes
+                {box2}
               </Typography>
             </Box>
-            <Box  style={{width:"32%", padding:"1rem"}}>
+            <Box style={{ width: '32%', padding: '1rem' }}>
               <Typography variant="h6" component="div">
-                Smarter collection
+                {SMtitle3}
               </Typography>
               <Typography variant="body1" component="div">
-                Set predefined sending conditions and get more reviews with our
-                Flow collection tool. Use SMS invites to follow up, and boost
-                review conversion by an average of 22%.
+                {box3}
               </Typography>
             </Box>
           </Box>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,80 @@ import CategoryShow from '../components/CategoryShow';
 import ConnectVendorsWithBuyers from '../components/ConnectVendorsWithBuyers';
 import HomeSlides from '../components/HomeSlides';
 import HomeProduct from '../components/HomeProduct';
+import Trust from '../images/trust.jpg';
+import Pattern from '../images/showcase.svg';
 
 const Home = () => {
   return (
     <Box>
       <Banner />
       <CategoryShow />
-      <HomeProduct/>
+      <HomeProduct
+        title={'Trust.'}
+        text1={
+          '  Reviews help consumers buy with confidence - and 93% of online shoppers say they impact their purchase decisions.'
+        }
+        text2={
+          ' Boost your brand’s conversion with trust signals at every point of contact.'
+        }
+        text3={
+          'SAL-MADJAREB helped us build trust with customers, which has had a positive impact on customer satisfaction and conversion.'
+        }
+        SMtitle1={'  Product reviews'}
+        SMtitle2={'  Product insights'}
+        SMtitle3={' Smarter collection'}
+        box1={
+          ' See greater conversion with product review throughout the buyer journey. The more feedback you collect, the greater the SEO benefits.'
+        }
+        box2={
+          'Enhance the shopping experience and help customers choose the right products for their needs with Reviewer attributes'
+        }
+        box3={
+          'Set predefined sending conditions and get more reviews with our Flow collection tool. Use SMS invites to follow up, and boost review conversion by an average of 22%.'
+        }
+        bkg1={'rgb(131,70,119)'}
+        bkg2={
+          'linear-gradient(286deg, rgba(131,70,119,1) 0%, rgba(249,86,79,1) 71%, rgba(230,230,230,1) 100%)'
+        }
+        backColor={'#fcc8c2'}
+        bigImg={Trust}
+        SMimg={Pattern}
+      />
       <ConnectVendorsWithBuyers />
       <WhyReviews />
-      <HomeProduct/>
+      <HomeProduct
+        title={'Advocacy.'}
+        text1={
+          '  Reviews help consumers buy with confidence - and 93% of online shoppers say they impact their purchase decisions.'
+        }
+        text2={
+          ' Boost your brand’s conversion with trust signals at every point of contact.'
+        }
+        text3={
+          'SAL-MADJAREB helped us build trust with customers, which has had a positive impact on customer satisfaction and conversion.'
+        }
+        SMtitle1={'  Product reviews'}
+        SMtitle2={'  Product insights'}
+        SMtitle3={' Smarter collection'}
+        box1={
+          ' See greater conversion with product review throughout the buyer journey. The more feedback you collect, the greater the SEO benefits.'
+        }
+        box2={
+          'Enhance the shopping experience and help customers choose the right products for their needs with Reviewer attributes'
+        }
+        box3={
+          'Set predefined sending conditions and get more reviews with our Flow collection tool. Use SMS invites to follow up, and boost review conversion by an average of 22%.'
+        }
+        bkg1={'rgb(121,210,32)'}
+        bkg2={
+          'linear-gradient(90deg, rgba(121,210,32,1) 0%, rgba(73,222,210,1) 42%, rgba(11,244,233,1) 68%, rgba(0,212,255,1) 100%)'
+        }
+        backColor={
+          'linear-gradient(90deg, rgba(224,251,32,1) 0%, rgba(223,227,51,1) 30%, rgba(212,223,68,1) 42%, rgba(252,255,105,1) 58%, rgba(238,255,65,1) 68%, rgba(218,225,58,1) 85%, rgba(255,239,0,1) 100%)'
+        }
+        bigImg={Trust}
+        SMimg={Pattern}
+      />
       <CallToAction />
       <JoinCommunity />
       <HomeSlides />
