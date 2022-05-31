f22953af44628526aac24d7bd201eca0af22b25a
BILLEL <billel.bahani@gmail.com>
2022-05-31 16:15:11 +0100

    building services page

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   Container,
   makeStyles,
@@ -10,47 +10,57 @@ import {
   Button,
   ButtonGroup,
   Box,
-} from "@material-ui/core";
+} from '@material-ui/core';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   serviceItem: {
-    width: "330px",
-    height: "330px",
-
-    borderRadius: "4px",
-    padding: "2.5rem",
-    backgroundColor: "#fff",
+    width: '330px',
+    height: '330px',
+    borderRadius: '4px',
+    padding: '2.5rem',
+    backgroundColor: '#fff',
     boxShadow:
-      "0px 2px 1px -1px rgba(0,0,0,.2) ,0px 1px 1px 0px rgba(0,0,0,.14),0px 1px 3px 0px rgba(0,0,0,.12)",
+      '0px 2px 1px -1px rgba(0,0,0,.2) ,0px 1px 1px 0px rgba(0,0,0,.14),0px 1px 3px 0px rgba(0,0,0,.12)',
+    margin: '1rem 0',
   },
   serviceItemHeader: {
-      borderBottom:"1px solid #ccc",
-      paddingBottom:"1.5rem",
+    borderBottom: '1px solid #ccc',
+    paddingBottom: '1.5rem',
   },
-  serviceItemDetail:{
-      marginTop:"1rem"  }
+  serviceItemDetail: {
+    marginTop: '1rem',
+  },
 }));
 
 const ServiceItem = ({
-  title = "Invite",
-  subTitle = "this service is meant to deploy a marketing assets for",
+  title = 'Invite',
+  subTitle = 'this service is meant to deploy a marketing assets for',
   text,
 }) => {
   const classes = useStyles();
   return (
     <Box className={classes.serviceItem}>
       <Box className={classes.serviceItemHeader} gutterBottom>
-        <Typography variant="h4" component="div" style={{fontWeight:"600"}} gutterBottom>
+        <Typography
+          variant="h4"
+          component="div"
+          style={{ fontWeight: '600' }}
+          gutterBottom
+        >
           {title}
         </Typography>
         <Typography variant="body1" component="div">
           {subTitle}
         </Typography>
       </Box>
-      <Box className={classes.serviceItemDetail} >
-        <Typography variant="body1" component="div" style={{marginBottom:"2rem"}}>
+      <Box className={classes.serviceItemDetail}>
+        <Typography
+          variant="body1"
+          component="div"
+          style={{ marginBottom: '2rem' }}
+        >
           {text}
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
           similique praesentium, non soluta quam incidunt sequi blanditiis
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,16 +1,29 @@
-import React from "react";
-import { Box, makeStyles, Container, Typography } from "@material-ui/core";
-import SelectSearchBar from "../components/SelectSearchBar";
-import CustomerReview from "../images/customerReview.jpg";
-import CompaniezData from "../Donnes.json";
-import SearchBar from "../components/SearchBar";
-import PricingPlan from "../components/PricingPlan";
-import ServiceItem from "../components/ServiceItem";
+import React from 'react';
+import { Box, makeStyles, Container, Typography } from '@material-ui/core';
+import SelectSearchBar from '../components/SelectSearchBar';
+import CustomerReview from '../images/customerReview.jpg';
+import CompaniezData from '../Donnes.json';
+import SearchBar from '../components/SearchBar';
+import PricingPlan from '../components/PricingPlan';
+import ServiceItem from '../components/ServiceItem';
 
 const useStyles = makeStyles((theme) => ({
   services: {
-  padding:"10rem 0",
-    [theme.breakpoints.down("sm")]: {},
+    padding: '10rem 0',
+    [theme.breakpoints.down('sm')]: {},
+  },
+  itemzWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '10rem',
+  },
+  itemzdetail: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 }));
 
@@ -18,11 +31,33 @@ const Services = () => {
   const classes = useStyles();
 
   return (
-    <Box style={{ backgroundColor: "#fff" }}>
+    <Box>
       <Container className={classes.services}>
         <PricingPlan />
-
-        <ServiceItem/>
+        <Box className={classes.itemzWrapper}>
+          <Typography
+            variant="h1"
+            component={'div'}
+            style={{
+              width: '70%',
+              fontWeight: '600',
+              textAlign: 'center',
+              fontSize: '5rem',
+              padding: '6rem 0',
+              borderTop: '1px solid #ccc',
+            }}
+          >
+            Find more about our services
+          </Typography>
+          <Box className={classes.itemzdetail}>
+            <ServiceItem />
+            <ServiceItem />
+            <ServiceItem />
+            <ServiceItem />
+            <ServiceItem />
+            <ServiceItem />
+          </Box>
+        </Box>
       </Container>
     </Box>
   );
